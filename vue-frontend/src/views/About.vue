37950c8bd6dff7e3ad7e0a9deaa8968cd0f19b7a
nlukic97 <nlukic97@gmail.com>
2020-08-15 19:26:04 +0200

    changed design

--- vue-frontend/src/views/About.vue
+++ vue-frontend/src/views/About.vue
@@ -2,17 +2,40 @@
   <div class="about">
     <v-container>
       <v-row>
-        <v-col>
+        <v-col class="red white--text pt-8 pb-8">
           <h1>About</h1>
+          <p>Where it all starts...</p>
         </v-col>
       </v-row>
       <v-row>
-        <v-col sm="4" cols="12">E</v-col>
-        <v-col sm="4" cols="12">E</v-col>
-        <v-col sm="4" cols="12">E</v-col>
+        <v-col cols="12" sm="5" md="4" class="d-flex align-center">
+            <img src="../assets/sea.jpg" alt="" style="width:100%;">
+        </v-col>
+        <v-col cols="12" sm="7" md="8" class="text-left">
+          <h3>Where it all started...</h3>
+          <div>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia nam minus voluptate perspiciatis! Suscipit eum cupiditate, quasi, a voluptates veniam corporis libero eaque dolor fugit exercitationem, dignissimos aspernatur. Excepturi eligendi exercitationem tenetur aperiam animi sequi vero assumenda accusamus sunt explicabo non nam magnam deserunt doloribus nihil, atque pariatur ipsum id placeat? Similique possimus at distinctio voluptates laboriosam, sapiente tempora. Quas iste incidunt tempora nostrum excepturi unde quae placeat harum quidem sint suscipit, voluptatum quos sed assumenda pariatur eaque. Itaque, quis aspernatur. Error ratione magnam repudiandae quae? Repellat nisi impedit excepturi!
+          </div>
+        </v-col>
       </v-row>
+
       <v-row>
-        <v-col class="text-left">
+        <v-col cols="7" class="text-left">
+          <h3>Where it all started...</h3>
+          <div>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia nam minus voluptate perspiciatis! Suscipit eum cupiditate, quasi, a voluptates veniam corporis libero eaque dolor fugit exercitationem, dignissimos aspernatur. Excepturi eligendi exercitationem tenetur aperiam animi sequi vero assumenda accusamus sunt explicabo non nam magnam deserunt doloribus nihil, atque pariatur ipsum id placeat? Similique possimus at distinctio voluptates laboriosam, sapiente tempora. Quas iste incidunt tempora nostrum excepturi unde quae placeat harum quidem sint suscipit, voluptatum quos sed assumenda pariatur eaque. Itaque, quis aspernatur. Error ratione magnam repudiandae quae? Repellat nisi impedit excepturi!
+          </div>
+        </v-col>
+        <v-col cols="5" class="d-flex align-center">
+            <img src="../assets/park-food.jpg" alt="" style="width:100%;">
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col cols="4" class="d-flex align-center">
+            <img src="../assets/park-food.jpg" alt="" style="width:100%;">
+        </v-col>
+        <v-col cols="8" class="text-left">
           <h3>Where it all started...</h3>
           <div>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia nam minus voluptate perspiciatis! Suscipit eum cupiditate, quasi, a voluptates veniam corporis libero eaque dolor fugit exercitationem, dignissimos aspernatur. Excepturi eligendi exercitationem tenetur aperiam animi sequi vero assumenda accusamus sunt explicabo non nam magnam deserunt doloribus nihil, atque pariatur ipsum id placeat? Similique possimus at distinctio voluptates laboriosam, sapiente tempora. Quas iste incidunt tempora nostrum excepturi unde quae placeat harum quidem sint suscipit, voluptatum quos sed assumenda pariatur eaque. Itaque, quis aspernatur. Error ratione magnam repudiandae quae? Repellat nisi impedit excepturi!
